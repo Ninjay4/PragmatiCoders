572a564c0d55360c720d9e2325fc34b55041adab
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-25 23:28:38 -0500

    Update BasicCalculator.py
    Created a header and cleaned up the code

--- Python/BasicCalculator.py
+++ Python/BasicCalculator.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
-# PractiCoders Python Challenge
-# Round 1 Practice Problem #3
-# Write a function that takes two numbers
-# And a mathematical operation
-# (add, subtract, multiply, division)
-# As parameters and returns the result of that operation
-
-
-# In[2]:
-
-
-# Hint: Use if-elif-else statements
-# To perform the operation based on the given string
-
-
-# In[3]:
-
+'''
+File Name: BasicCalculator
+Created by: Ninjay4
+Created on: 2/21/24
+Modified on: 2/25/24
+Description:  PractiCoders Python Challenge
+Round 1 Practice Problem #3
+Write a function that takes two numbers
+And a mathematical operation
+(add, subtract, multiply, division)
+As parameters and returns the result of that operation
+Hint: Use if-elif-else statements
+To perform the operation based on the given string
+'''
 
 # Create 'calculator' Function
 def calculator(num1, num2, operation):
@@ -33,18 +26,10 @@ def calculator(num1, num2, operation):
     elif operation == 'subtract':
         return (num1 - num2)
 
-
-# In[4]:
-
-
 # Create input parameters
 num1 = int(input("Please enter the 1st number: "))
 num2 = int(input("Please enter the 2nd number: "))
 operation = input("Please enter the operation with only one of the following: 'add', 'divide', 'multiply', or 'substract' ")
-
-
-# In[5]:
-
 
 # Run the calculator function
 calculator(num1, num2, operation)
